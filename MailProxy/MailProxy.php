285540a9b5e39e4f1c6bea62900cafa0eb424728
schoolphp <inpost@list.ru>
2017-01-27 21:12:25 +0200

    Правки

--- MailProxy/MailProxy.php
+++ MailProxy/MailProxy.php
@@ -40,39 +40,9 @@ class MailProxy extends \PHPMailer\PHPMailer\PHPMailer {
 		}
 
 		$key = time();
-		$hash = myHash($this->to[0][0].preg_replace('#^.{2}(.+).{2}$#u',"\\1",$key));
-		$unsubscribe = 'https://school-php.com/login/unsubscribe?email='.urlencode($this->to[0][0]).'&amp;key='.urlencode($key).'&amp;hash='.urlencode($hash);
-		$this->addCustomHeader("List-Unsubscribe",'<mailto:unsubscribe@school-php.com>, <'.$unsubscribe.'>');
-		$this->addAttachment('./skins/img/logo.png');
-		$this->addAttachment('./skins/img/edu4.png');
-		$this->addAttachment('./skins/img/mailbg.png');
-
-		$message = '<!DOCTYPE html>
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>'.hc($this->Subject).'</title>
-</head>
-<body>
-<div style="background-color:#eee; padding:20px;">
-<div style="background:white url(https://school-php.com/skins/img/mailbg.png) no-repeat top right;border: 1px solid #dfdfdf;width: 560px;padding: 0px 25px 25px;margin: 0 auto;">
-<div role="banner" style="padding:15px;border-bottom: 1px solid #eee;"><h1 style="margin-bottom:0px; padding-bottom:0px;"><img src="https://school-php.com/skins/img/logo.png" alt="Школа программирования"><br>Школа PHP Программирования</h1></div>
-<div role="main" style="margin: 30px 10px 50px 10px; font-size: 14px; line-height: 1.5;">
-'.$message.'
-</div>
-<div role="footer" style="padding-top:10px; border-top: 1px solid #eee;">
-<p>
-Автоматическая система рассылок school-php.com!<br>
-Вы можете отписаться от писем пройдя по следующей ссылке:<br>
-<a href="'.$unsubscribe.'">'.$unsubscribe.'</a>
-</p>
-</div>
-</div>
-<div align="center"><img src="https://school-php.com/skins/img/edu4.png"></div>
-</div>
-</body>
-</html>';
-
+		$hash = \myHash($this->to[0][0].preg_replace('#^.{2}(.+).{2}$#u',"\\1",$key));
+		$unsubscribe = \Core::$DOMAIN.'/login/unsubscribe?email='.urlencode($this->to[0][0]).'&amp;key='.urlencode($key).'&amp;hash='.urlencode($hash);
+		$this->addCustomHeader("List-Unsubscribe",'<mailto:'.\Core::$ADMIN.'>, <'.$unsubscribe.'>');
 		return parent::msgHTML($message, $basedir, $advanced);
 	}
 }
