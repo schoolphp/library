0bbcc81fe1a88127ceaf83d4637c173fff6c7e0d
school-php <inpost@list.ru>
2018-05-08 22:00:24 +0300

    Правки

--- Uploader/UploaderInterface.php
+++ Uploader/UploaderInterface.php
@@ -4,5 +4,11 @@ namespace FW\Uploader;
 interface UploaderInterface
 {
 	public function __construct($file,$options);
+
+	/**
+	 * @param mixed $to
+	 * @param array $options
+	 * @return $this
+	 */
 	public function save($to = false,$options = []);
 }
